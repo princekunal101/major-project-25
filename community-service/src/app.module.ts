e4da425eb4f2689afcedc32b2ecc5af843d289d1
Prince Kunal <princekunal652@gmail.com>
2025-11-16 01:54:18 +0530

    auth guard added

--- community-service/src/app.module.ts
+++ community-service/src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommunitiesModule } from './communities/communities.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
-  imports: [PrismaModule, CommunitiesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config) => ({
+        secret: config.get('jwt.secret'),
+      }),
+      global: true,
+      inject: [ConfigService],
+    }),
+    PrismaModule,
+    CommunitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
