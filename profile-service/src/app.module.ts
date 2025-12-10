14e58a6e5aee81bc073e5bc334652a6f32870e16
Prince Kunal <princekunal652@gmail.com>
2025-12-10 15:50:10 +0530

    enhance authentication with JWT and microservices integration

--- profile-service/src/app.module.ts
+++ profile-service/src/app.module.ts
@@ -3,13 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProfilesModule } from './profiles/profiles.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import config from './config/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+      load: [config],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config) => ({
+        secret: config.get('jwt.secret'),
+      }),
+      global: true,
+      inject: [ConfigService],
     }),
     PrismaModule,
     ProfilesModule,
--- profile-service/src/profiles/profiles.module.ts
+++ profile-service/src/profiles/profiles.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { ProfilesController } from './profiles.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.TCP,
+        options: { host: '127.0.0.1', port: 8877 },
+      },
+    ]),
+  ],
   controllers: [ProfilesController],
   providers: [ProfilesService],
 })
